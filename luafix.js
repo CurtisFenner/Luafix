78532f3c6767d488fc49a7cb6ceef51e9481750b
CurtisFenner <cfenneremail@gmail.com>
2014-11-05 14:17:21 -0500

    Reference Addition to vread

--- luafix.js
+++ luafix.js
@@ -329,11 +329,22 @@ function vwrite(vars,iden,value,level) {
 	vars[level][name] = value;
 }
 
-function vread(vars,iden) {
+
+// vars the variable database
+// iden the identifier expression (MemberExpression or Identifier or the like)
+// ref a boolean to return a function resulting in the type --
+// used for passing values by reference
+function vread(vars,iden,ref) {
 	var name = nameFromIden(iden);
 	for (var i = vars.length - 1; i >= 0; i--) {
 		if (vars[i][name]) {
-			return vars[i][name];
+			if (ref) {
+				return (function() {
+					return vars[i][name];
+				});
+			} else {
+				return vars[i][name];
+			}
 		}
 	}
 	return "nil";
